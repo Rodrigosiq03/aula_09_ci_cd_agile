7a49d5c8f6ce7b72f96f633142e45659823257ba
Rodrigo <rodrigo.dsiqueira1@gmail.com>
2023-05-08 08:42:44 -0300

    TDD - Applying the principle to start with tests

--- test_methods.py
+++ test_methods.py
@@ -1,5 +1,5 @@
 import methods
- 
+
 def test_area():
     # given a width of 2 and a height of 5
     width = 2
@@ -10,7 +10,7 @@ def test_area():
 
     # then the area should be 10
     assert output == 10
- 
+
 def test_perimeter():
     # given a width of 2 and a height of 5
     width = 2
@@ -21,3 +21,35 @@ def test_perimeter():
     
     # then the perimeter should be 14
     assert output == 14
+
+def test_sub():
+    value1 = 10
+    value2 = 5
+    
+    output = methods.sub(value1, value2)
+    
+    assert output == 5
+    
+def test_add():
+    value1 = 10
+    value2 = 5
+    
+    output = methods.add(value1, value2)
+    
+    assert output == 15
+    
+def test_div():
+    value1 = 10
+    value2 = 5
+    
+    output = methods.div(value1, value2)
+    
+    assert output == 2
+
+def test_mult():
+    value1 = 10
+    value2 = 5
+    
+    output = methods.mult(value1, value2)
+    
+    assert output == 50
