a6318626309e3231df7550029a52bd83d9ab0383
Rodrigo <rodrigo.dsiqueira1@gmail.com>
2023-05-08 08:45:08 -0300

    TDD - After doing tests I did the implementation

--- methods.py
+++ methods.py
@@ -1,7 +1,23 @@
 def area_of_rectangle(width, height):
     area = width*height
     return area
- 
+
 def perimeter_of_rectangle(width, height):
     perimeter = 2 * (width + height)
     return perimeter
+
+def sub(value1, value2):
+    sub = value1 - value2
+    return sub
+
+def add(value1, value2):
+    add = value1 + value2
+    return add
+
+def div(value1, value2):
+    div = value1 / value2
+    return div
+
+def mult(value1, value2):
+    mult = value1 * value2
+    return mult
